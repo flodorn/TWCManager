e7913d2cf37e92bee3eb0108e2ad90e5bb9d4c60
flodorn <44878709+flodorn@users.noreply.github.com>
2019-04-11 21:04:43 +0200

    Update TWCManager.py

--- TWCManager.py
+++ TWCManager.py
@@ -1885,12 +1885,14 @@ class TWCSlave:
 
         now = time.time()
         self.timeLastRx = now
+        
+        TWCIDtoString = ('TWC/ampsMax/' + hex_str(self.TWCID))
 
         self.reportedAmpsMax = ((heartbeatData[1] << 8) + heartbeatData[2]) / 100
-        transmit_mqtt("TWC/ampsMax/" + hex_str(self.TWCID), self.reportedAmpsMax)
+        transmit_mqtt(TWCIDtoString, self.reportedAmpsMax)
     
         self.reportedAmpsActual = ((heartbeatData[3] << 8) + heartbeatData[4]) / 100
-        transmit_mqtt('TWC/power/' + hex_str(self.TWCID), self.reportedAmpsActual)
+        transmit_mqtt('TWC/power/' + self.TWCID, self.reportedAmpsActual)
 	
         self.reportedState = heartbeatData[0]
         transmit_mqtt(('TWC/state/' + hex_str(self.TWCID)), self.reportedState)
