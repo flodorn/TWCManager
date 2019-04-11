d2dc1603602d653466b02b5e37f0dfcf22f20589
flodorn <44878709+flodorn@users.noreply.github.com>
2019-04-11 22:56:51 +0200

    Update TWCManager.py

--- TWCManager.py
+++ TWCManager.py
@@ -959,46 +959,7 @@ class TWCSlave:
         if(len(overrideMasterHeartbeatData) >= 7):
             self.masterHeartbeatData = overrideMasterHeartbeatData
 
-        if(self.protocolVersion == 2):
-            # TODO: Start and stop charging using protocol 2 commands to TWC
-            # instead of car api if I ever figure out how.
-            if(self.lastAmpsOffered == 0 and self.reportedAmpsActual > 4.0):
-                # Car is trying to charge, so stop it via car API.
-                # car_api_charge() will prevent telling the car to start or stop
-                # more than once per minute. Once the car gets the message to
-                # stop, reportedAmpsActualSignificantChangeMonitor should drop
-                # to near zero within a few seconds.
-                # WARNING: If you own two vehicles and one is charging at home but
-                # the other is charging away from home, this command will stop
-                # them both from charging.  If the away vehicle is not currently
-                # charging, I'm not sure if this would prevent it from charging
-                # when next plugged in.
-                queue_background_task({'cmd':'charge', 'charge':False})
-                if(debugLevel >= 10):
-                    print(time_now() + ': BUGFIX: lastAmpsOffered = 0 and reportedAmpsActual > 4')
-            elif(self.lastAmpsOffered >= 5.0 and self.reportedAmpsActual < 2.0
-                 and self.reportedState != 0x02
-            ):
-                # Car is not charging and is not reporting an error state, so
-                # try starting charge via car api.
-                queue_background_task({'cmd':'charge', 'charge':True})
-                if(debugLevel >= 10):
-                    print(time_now() + ': BUGFIX: lastAmpsOffered >= 5 and reportedAmpsActual < 2')
-            elif(self.reportedAmpsActual > 4.0):
-                # At least one plugged in car is successfully charging. We don't
-                # know which car it is, so we must set
-                # vehicle.stopAskingToStartCharging = False on all vehicles such
-                # that if any vehicle is not charging without us calling
-                # car_api_charge(False), we'll try to start it charging again at
-                # least once. This probably isn't necessary but might prevent
-                # some unexpected case from never starting a charge. It also
-                # seems less confusing to see in the output that we always try
-                # to start API charging after the car stops taking a charge.
-                for vehicle in carApiVehicles:
-                    vehicle.stopAskingToStartCharging = False
-                if(debugLevel >= 10):
-                    print(time_now() + ': BUGFIX: reportedAmpsActual > 4')
-
+       
         send_msg(bytearray(b'\xFB\xE0') + fakeTWCID + bytearray(self.TWCID)
                  + bytearray(self.masterHeartbeatData))
 
@@ -1877,12 +1838,7 @@ while True:
                         webResponseMsg = hex_str(lastTWCResponseMsg)
                     else:
                         webResponseMsg = 'None'
-                elif(webMsg[0:20] == b'carApiEmailPassword='):
-                    m = re.search(b'([^\n]+)\n([^\n]+)', webMsg[20:len(webMsg)], re.MULTILINE)
-                    if(m):
-                        queue_background_task({'cmd':'carApiEmailPassword',
-                                                  'email':m.group(1).decode('ascii'),
-                                                  'password':m.group(2).decode('ascii')})
+
                 elif(webMsg[0:23] == b'setMasterHeartbeatData='):
                     m = re.search(b'([0-9a-fA-F]*)', webMsg[23:len(webMsg)], re.MULTILINE)
                     if(m):
